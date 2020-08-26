1593bb13b856c823a8c74e340958d9fe9e0aaef3
Srinu <kotyadapoornimasrinivas@gmail.com>
2020-08-26 20:27:11 +0530

    "6th commit"

--- lib/util/pose_description.dart
+++ lib/util/pose_description.dart
@@ -29,3 +29,5 @@ List<String> beginnerdescription = [
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
 ];
+
+
